5d2dbbe8703401489ba554965161234ad1681207
christian <cc419378878@gmail.com>
2018-03-26 16:58:24 +0800

    wget-test

--- main.js
+++ main.js
@@ -1,41 +1,36 @@
-const request = require('superagent');
-const schedule = require('node-schedule');
-const fs = require('fs');
 const exec = require('child_process').exec;
+const config = require('./config');
 
-const test_urls = ['https://www.google.com/', 'https://www.youtube.com/', 'https://www.sina.com.cn/'];
-const urls = ['http://g95.222-224-140.ppp.wakwak.ne.jp:62462/api/iphone/', 'http://61.75.128.225:3068/api/iphone/', ' http://61.107.179.232:22980/api/iphone/', ' http://59.3.208.17:43950/api/iphone/', 'http://147.46.146.165:55866/api/iphone/'];
+// const test_urls = ['https://www.google.com/', 'https://www.youtube.com/', 'https://www.baidu.com/'];
+const urls = [].concat(config.site.main, config.site.mirrors);
 const flags = {
-  i: 0,
-  len: test_urls.length
+  i: 0
 };
 
+let new_urls = urls.map(x => x + 'api/iphone/');
+
 function getJson(urls, flags) {
+  const len = urls.length;
   let i = flags.i;
-  let len = flags.len;
-  let url = urls[i]
-  console.log('url is: ' + url);
-  request
-    .get(url)
-    .timeout({
-      response: 10000,
-      deadline: 10000,
-    })
-    .end((error, res) => {
-      if(error) {
-        console.log(`can't connect ${url}!`);
-        i < len ? getJson(urls, flags) : console.log(`can't connect all urls!`);
-        i++;
-        flags.i = i;
-      } else{
-        fs.writeFile('./api/iphone', JSON.stringify(res.text), (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!');
-          pushGithub();
-        });
-        console.log(`${url} is connected now!`);
+  let url = urls[i];
+
+  exec(`wget --output-document='./api/iphone' --tries=2 --timeout=5 ${url}`, (err, stdout, stderr) => {
+    if(err) {
+      console.log(`Can't connect ${url}!`);
+      flags.i++;
+      if(flags.i < len) {
+        getJson(urls, flags);
+      } else {
+        console.log('All connect deny!');
       }
-    });
+    } else {
+      pushGithub();
+      console.log('Success!');
+    }
+    console.log(`stdout is: ${stdout}`);
+    console.log(`stdout is: ${stderr}`);
+  });
+
 }
 
 function pushGithub() {
@@ -48,7 +43,11 @@ function pushGithub() {
 
   // 如果iphone有改变再执行其他命令
   exec(diff, (err, stdout, stderr) => {
-    console.log(!stdout);
+    if (err){
+      console.log(err);
+      return;
+    }
+    console.log(stdout);
     console.log(stderr);
     if(!stdout) {
       exec(add);
@@ -58,10 +57,5 @@ function pushGithub() {
   })
 }
 
-getJson(urls, flags);
 
-
-//定时任务
-// schedule.scheduleJob('16 * * * * *', function(){
-//   console.log('The answer to life, the universe, and everything!');
-// });
+getJson(new_urls, flags);
